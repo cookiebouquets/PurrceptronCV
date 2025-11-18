cded7759540756f070e317ddb088c5a180389bfd
cookiebouquets <silva@bemaniso.ws>
2025-11-17 20:50:36 -0500

    gen dataset

---
+++ data processing/datagen.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+
+path = "IMG_2515.mov"
+save_dir = r"..\frames\empty"
+
+os.makedirs(save_dir,exist_ok=True)
+
+
+cap = cv2.VideoCapture(path)
+frame_num = 0
+print("Opened:", cap.isOpened())
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+
+    if frame_num % 5 == 0:
+        cv2.imwrite(f"{save_dir}/frame_{frame_num:05d}.jpg", frame)
+    
+    frame_num += 1
+
+cap.release()
